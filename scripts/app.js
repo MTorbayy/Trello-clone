b602beb54c74c40777e3fbf9db1ad2adb1b5a8b5
Marie Torbay <marie.torbay@gmail.com>
2022-02-18 15:08:25 +0100

    added toggle add Item

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,55 @@
 "use strict";
 const itemsContainer = document.querySelectorAll('.items-container');
+let actualContainer, actualBtn, actualUl, actualForm, actualTextInput, actualValidation;
+// Container général de toutes les fonctions addEventListener
 function addContainerListeners(currentContainer) {
     const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn');
+    const currentAddItemBtn = currentContainer.querySelector('.add-item-btn');
     deleteBtnListeners(currentContainerDeletionBtn);
+    addItemBtnListeners(currentAddItemBtn);
 }
+//Appel de chaque container avec la fonction précédente
 itemsContainer.forEach((container) => {
     addContainerListeners(container);
 });
+//Fonctions EventListeners
 function deleteBtnListeners(btn) {
     btn.addEventListener('click', handleContainerDeletion);
 }
+function addItemBtnListeners(btn) {
+    btn.addEventListener('click', handleAddItem);
+}
+//Fonctions déclenchées par les évènements
 function handleContainerDeletion(e) {
     const btn = e.target;
     const btnsArray = [...document.querySelectorAll('.delete-container-btn')];
     const containers = [...document.querySelectorAll('.items-container')];
     containers[btnsArray.indexOf(btn)].remove();
+}
+function handleAddItem(e) {
+    const btn = e.target;
+    if (actualContainer) {
+        toggleForm(actualBtn, actualForm, false)
+    }
+    setContainerItem(btn);
+    toggleForm(actualBtn, actualForm, true);
+}
+function toggleForm(btn, form, action) {
+    if (!action) {
+        form.style.display = "none";
+        btn.style.display = "block";
+    }
+    else if (action) {
+        form.style.display = "block";
+        btn.style.display = "none";
+    }
+}
+//Fonction permettant de mettre tous les éléments du container courant dans des variables pour pouvoir les utiliser
+function setContainerItem(btn) {
+    actualBtn = btn;
+    actualContainer = btn.parentElement;
+    actualUl = actualContainer.querySelector('ul');
+    actualForm = actualContainer.querySelector('form');
+    actualTextInput = actualContainer.querySelector('input');
+    actualValidation = actualContainer.querySelector('.validation-msg');
 }
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,76 @@
 const itemsContainer = document.querySelectorAll('.items-container') as NodeListOf<HTMLDivElement>
 
+let actualContainer: HTMLDivElement,
+    actualBtn: HTMLButtonElement,
+    actualUl: HTMLUListElement,
+    actualForm: HTMLFormElement,
+    actualTextInput: HTMLInputElement,
+    actualValidation: HTMLSpanElement
 
+
+// Container général de toutes les fonctions addEventListener
 
 function addContainerListeners(currentContainer: HTMLDivElement) {
 
-    const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn') as HTMLButtonElement;
+    const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn') as HTMLButtonElement
+    const currentAddItemBtn = currentContainer.querySelector('.add-item-btn') as HTMLButtonElement
 
     deleteBtnListeners(currentContainerDeletionBtn)
+    addItemBtnListeners(currentAddItemBtn)
 
 }
+
+//Appel de chaque container avec la fonction précédente
 
 itemsContainer.forEach((container: HTMLDivElement) => {
     addContainerListeners(container)
 })
 
+//Fonctions EventListeners
+
 function deleteBtnListeners(btn: HTMLButtonElement) {
     btn.addEventListener('click', handleContainerDeletion)
 }
+
+function addItemBtnListeners(btn: HTMLButtonElement) {
+    btn.addEventListener('click', handleAddItem)
+}
+
+//Fonctions déclenchées par les évènements
 
 function handleContainerDeletion(e : MouseEvent) {
     const btn = e.target as HTMLButtonElement;
     const btnsArray = [...document.querySelectorAll('.delete-container-btn')] as HTMLButtonElement[];
     const containers = [...document.querySelectorAll('.items-container')] as HTMLDivElement[];
     containers[btnsArray.indexOf(btn)].remove()
+}
+
+function handleAddItem(e: MouseEvent) {
+    const btn = e.target as HTMLButtonElement
+    if(actualContainer) {
+        actualContainer.style.display = "none"
+    }
+    setContainerItem(btn)
+    toggleForm(actualBtn, actualForm, true)
+}
+
+function toggleForm(btn: HTMLButtonElement, form: HTMLFormElement, action: boolean) {
+    if(!action) {
+        form.style.display = "none"
+        btn.style.display = "block"
+    } else if (action) {
+        form.style.display = "block"
+        btn.style.display = "none"
+    }
+}
+
+//Fonction permettant de mettre tous les éléments du container courant dans des variables pour pouvoir les utiliser
+
+function setContainerItem(btn: HTMLButtonElement) {
+    actualBtn = btn
+    actualContainer = btn.parentElement as HTMLDivElement
+    actualUl = actualContainer.querySelector('ul') as HTMLUListElement
+    actualForm = actualContainer.querySelector('form') as HTMLFormElement
+    actualTextInput = actualContainer.querySelector('input') as HTMLInputElement
+    actualValidation = actualContainer.querySelector('.validation-msg') as HTMLSpanElement
 }
